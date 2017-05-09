cd71d7cf281d1793bf697f89cd67f4a8e1f79e49
woodinnis <wood.innis@gmail.com>
2017-05-09 12:52:36 -0700

    First scene, UI, objects, and script created
    _scene00 added
    UI Canvas and UI text objects added to scene
    GameObject prefab goPan created
    pan.cs created, added to goPan

---
+++ Assets/Scripts/Pan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Pan : MonoBehaviour {
+
+    
+    // Use this for initialization
+	void Start () {
+        Text text = FindObjectOfType<Text>();
+
+        text.text = "I'm a pan";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
