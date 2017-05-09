911c8899311ab1ebb8148da0aca46d2767f26bca
woodinnis <wood.innis@gmail.com>
2017-05-09 15:28:22 -0700

    Added bacon object
    GameObject prefab goBacon created
    Two sprites for raw and cooked states added
    Basic script created

---
+++ Assets/Scripts/Bacon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bacon : MonoBehaviour {
+
+    public Sprite[] baconSprites;
+    public int rawIndex = 0;
+    public int cookedIndex;
+
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.sprite = baconSprites[rawIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sprite.sprite = baconSprites[cookedIndex];
+        }
+    }
+}
