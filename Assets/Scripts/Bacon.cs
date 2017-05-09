af3828eec01d787b37553ce520e3ce092b9efc0c
woodinnis <wood.innis@gmail.com>
2017-05-09 16:22:11 -0700

    Bacon position stays relative to pan
    goBacon checks transform.position of goPan
    goBacon maintains its position on screen relative to goPan regardless of
    movement

--- Assets/Scripts/Bacon.cs
+++ Assets/Scripts/Bacon.cs
@@ -9,16 +9,27 @@ public class Bacon : MonoBehaviour {
     public int cookedIndex;
 
     private SpriteRenderer sprite;
+    private Transform panTransform;
+    private float yOffset;
 
+    void Awake()
+    {
+        yOffset = transform.position.y;
+    }
 	// Use this for initialization
 	void Start () {
         sprite = GetComponent<SpriteRenderer>();
         sprite.sprite = baconSprites[rawIndex];
+
+        panTransform = FindObjectOfType<Pan>().transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector3 v3 = panTransform.position;
+        v3.y = v3.y + yOffset;
+        transform.position = v3;
+
 	}
 
     void OnMouseOver()
