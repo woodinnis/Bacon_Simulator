798971e6c36d0bdf91051cc33bbea891d56a1d53
woodinnis <wood.innis@gmail.com>
2017-05-31 10:37:40 -0700

    Changes to PauseMenu.cs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,45 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
+    //  Public button variables for the pause menu buttons
+    public Button RestartButton;
+    public Button ResumeButton;
+    public Button QuitButton;
+
     private Image panelImage;
+    
 
 	void Awake()
     {
         panelImage = GetComponent<Image>();
+
+        //  Add listeners for all pause menu buttons
+        //RestartButton.onClick.AddListener(restartButtonClicked);
+        //ResumeButton.onClick.AddListener(resumeButtonClicked);
+        QuitButton.onClick.AddListener(quitButtonClicked);
     }
 
     void Start()
     {
         panelImage.enabled = false;
+    }
+
+    //  Functions performed when the corresponding pause menu button is clicked
+    //  Restart
+    void restartButtonClicked()
+    {
+        //  To be filled in
+    }
+
+    // Resume
+    void resumeButtonClicked()
+    {
+        //  To be filled in
+    }
+
+    //  Quit
+    void quitButtonClicked()
+    {
+        Debug.Log("OW! Stop that!");
     }
 }
