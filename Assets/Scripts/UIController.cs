4ad8408f852c34875c2e7713afd1e0ab3b2cf799
woodinnis <wood.innis@gmail.com>
2017-05-31 11:38:34 -0700

    UI Controller changes
    Added two elements to UIController.cs
    PauseButton
    PauseMenu
    
    Disabled UI elements from earlier versions
    Created new Button_Pause and linked to UIController
    Linked Panel_Pause to UIController

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,8 +4,18 @@ using System.Collections;
 
 public class UIController : MonoBehaviour {
 
+    public Button pauseButton;
+    public PauseMenu pauseMenu;
+
 	// Use this for initialization
 	void Start () {
-	
+
+        pauseButton.onClick.AddListener(pauseButtonClicked);
+        pauseMenu.enabled = false;
 	}
+
+    void pauseButtonClicked()
+    {
+        Debug.Log("You clicked me!");
+    }
 }
