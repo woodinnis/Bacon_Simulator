8e849dcf0fb018e83ce7734d580c344c741ea2a2
woodinnis <wood.innis@gmail.com>
2017-06-08 11:59:08 -0700

    Start Button
    Script created to handle start button functions
    - Basic script connections tested

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+    private Button startButton;
+
+	// Use this for initialization
+	void Start () {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(startButtonClicked);
+	}
+
+    void startButtonClicked()
+    {
+        Debug.Log("I did a thing");
+    }
+}
