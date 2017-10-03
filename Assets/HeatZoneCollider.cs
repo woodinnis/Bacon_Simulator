29f425fc940bb2c8d5de4185a1a5164f12f76ec8
woodinnis <wood.innis@gmail.com>
2017-10-03 14:04:50 -0700

    HeatZone Collider reduced to a single prefab
    Created a single HeatZone prefab, and added HeatZoneCollider.cs to
    manage adjustable radius and heat levels.

---
+++ Assets/HeatZoneCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeatZoneCollider : MonoBehaviour {
+
+    public float radius = 0.0f;
+    public float heatMultiplier= 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<CircleCollider2D>().radius = radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
