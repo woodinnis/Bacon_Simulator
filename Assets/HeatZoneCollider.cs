e6474ec216d6b78f813911e707324b0ae4a37e85
woodinnis <wood.innis@gmail.com>
2017-10-04 16:21:48 -0700

    Bacon now cooks according to Heat Zones
    Bacon now cooks at different rates according to the Heat Zone where it
    is placed. ResetBaconCookTime() adjusts the cooked time and burned time
    based on the collider.
    
    Created private floats to cache the initial values set in the inspector,
    used for reseting cooked/burned times
    
    Cleanup

--- Assets/HeatZoneCollider.cs
+++ Assets/HeatZoneCollider.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class HeatZoneCollider : MonoBehaviour {
 
     public float radius = 0.0f;
+    [Tooltip("Multiply the bacon cooktime by this value")]
     public float heatMultiplier= 0.0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         GetComponent<CircleCollider2D>().radius = radius;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
