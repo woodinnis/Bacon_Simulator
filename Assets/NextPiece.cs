462a38d0d3a2f12539e87c0a82e4004725fdcfbd
woodinnis <wood.innis@gmail.com>
2017-10-06 15:37:43 -0700

    NextPieceBox rebuild
    Begin new branch to rebuild the respawn functions and create a new Next
    Piece box

---
+++ Assets/NextPiece.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextPiece : MonoBehaviour {
+
+    public float offset
+    { get; set; }
+    public bool occupied
+    { get; set; }
+    public Vector3 position
+    { get; set; }
+}
